285a7d03d274f63d807d0ad20ebd6d832e0452ac
Lisandro Alarcón <aurealinbox@gmail.com>
2021-03-04 19:44:52 -0300

    Update build-win-workflow.bat

--- build-win-workflow.bat
+++ build-win-workflow.bat
@@ -6,9 +6,9 @@ set LGW_INCLUDE_DIR="%3"
 
 mkdir build
 @echo on
-c++ -c src/geebly.cpp -o build/geebly.o -I%SDL2_INCLUDE_DIR% -I%LGW_INCLUDE_DIR% -std=c++2a -Ofast -Wno-format -Wno-narrowing -D GEEBLY_VERSION_MAJOR=0 -D GEEBLY_VERSION_MINOR=7 -D GEEBLY_VERSION_CLASS=b -D GEEBLY_COMMIT_ID=50e4056
+c++ -c src/geebly.cpp -o build/geebly.o -I%SDL2_INCLUDE_DIR% -I%LGW_INCLUDE_DIR% -std=c++2a -Ofast -Wno-format -Wno-narrowing -m64 -D GEEBLY_VERSION_MAJOR=0 -D GEEBLY_VERSION_MINOR=7 -D GEEBLY_VERSION_CLASS=b -D GEEBLY_COMMIT_ID=50e4056
 
-c++ build/geebly.o -o build/geebly.exe -L%SDL2_LIB_DIR% -lSDL2 -lSDL2main
+c++ build/geebly.o -o build/geebly.exe -L%SDL2_LIB_DIR% -m64 -lSDL2 -lSDL2main
 @echo off
 
 del "build\geebly.o"
