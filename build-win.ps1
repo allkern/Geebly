535980ec7eab4a7e0b9fcfdd71471994377191f1
Lisandro Alarcón <aurealinbox@gmail.com>
2021-03-09 22:57:04 -0300

    Update build-win.ps1

--- build-win.ps1
+++ build-win.ps1
@@ -8,6 +8,6 @@ md -Force -Path build > $null
 
 c++ -c src/geebly.cpp -o build/geebly.o -I"`"$($SDL2_INCLUDE_DIR)`"" -I"`"$($LGW_INCLUDE_DIR)`"" -std=c++2a -m64 -Ofast -Wno-format -Wno-narrowing -D GEEBLY_VERSION_TAG=$VERSION_TAG -D GEEBLY_COMMIT_HASH=$COMMIT_HASH
 
-c++ build/geebly.o -o build/geebly.exe -L"`"$($SDL2_LIB_DIR)`"" -m64 -lSDL2main -lSDL2
+c++ build/geebly.o -o build/geebly.exe -L"`"$($SDL2_LIB_DIR)`"" -m64 -lmingw32 -lSDL2main -lSDL2
 
 del "build\geebly.o"
